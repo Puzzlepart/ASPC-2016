9c140cc376fdd1549f9c1f480c069ba490ec61b5
Kimzter <carl.joakim.damsleth@puzzlepart.com>
2016-02-25 14:47:57 +0100

    removed bower components

--- bower_components/js-sites-core/src/schema/INavigationNode.d.ts
+++
@@ -1,7 +0,0 @@
-declare module Pzl.Sites.Core.Schema {
-    interface INavigationNode {
-        Title: string;
-        Url: string;
-        Children:Array<INavigationNode>;        
-    }
-}
--- bower_components/js-sites-core/src/utilities/utilities.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="Logger.ts" />
-/// <reference path="RestHelper.ts" />
-/// <reference path="Sequencer.ts" />
-/// <reference path="TokenParser.ts" />
