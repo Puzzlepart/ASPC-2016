7f78b920ba03516b1e010d5108bde1892420db28
olemp <olemp@puzzlepart.com>
2016-02-25 18:06:18 +0100

    Config stuff

--- SiteAssets/js/pzl.config.ts
+++ SiteAssets/js/pzl.config.ts
@@ -1,11 +1,11 @@
 module Pzl.Config {
     export var PROVISIONING_LANGUAGE = 1044;
     export var PROVISIONING_WEBTEMPLATE = "STS#0";    
-    export var PROVISIONING_WAITMESSAGE_TEXT = "Vi arbeider med saken";    
-    export var PROVISIONING_WAITMESSAGE_DESCRIPTION = "Bør ikke ta så lang tid...";    
-    export var PROVISIONING_FAILED = "Problemer med å lage nytt arbeidsrom.";
+    export var PROVISIONING_WAITMESSAGE_TEXT = "Creating your room";    
+    export var PROVISIONING_WAITMESSAGE_DESCRIPTION = "";    
+    export var PROVISIONING_FAILED = "Problems creating your room";
     export var PROVISIONING_TEMPLATE_KEY = "_Pzl_Template";
     export var PROVISIONING_TEMPLATE_STAMPED_KEY = "_Pzl_Template_Applied";    
-    export var PROVISIONING_FAILED_TO_RETRIEVE_TEMPLATES = "Det skjedde en feil ved uthenting av tilgjenglige maler.";
+    export var PROVISIONING_FAILED_TO_RETRIEVE_TEMPLATES = "An error occcured retrieving templates.";
     export var ASSETS_DIR = "siteassets/pzl";
 }
