996f36c329ebb6f7249ad33fba25dede5a306bee
olemp <olemp@puzzlepart.com>
2016-02-26 17:45:04 +0100

    Adal stuff

--- SiteAssets/js/pzl.operationroom.ts
+++ SiteAssets/js/pzl.operationroom.ts
@@ -2,16 +2,19 @@
 /// <reference path="Controllers\controllers.d.ts" />
 /// <reference path="Services\services.d.ts" />
 
-EnsureScriptFunc('angular-google-maps.min.js', null, function() {
-    var app = angular.module("app", ['uiGmapgoogle-maps']).config(
-        ['uiGmapGoogleMapApiProvider', function(GoogleMapApiProviders) {
-            GoogleMapApiProviders.configure({
-                china: true
-            });
-        }])
-        .config(["$httpProvider", ($httpProvider) => {
-            $httpProvider.defaults.headers.common["Accept"] = "application/json; odata=verbose";
-        }])
-        .controller("operationSiteController", Controllers.OperationSiteController)
-    angular.bootstrap(document.getElementById('homeApp'), ['app']);
+
+EnsureScriptFunc('adal-angular.min.js', null, function() {
+    EnsureScriptFunc('angular-google-maps.min.js', null, function() {
+        var app = angular.module("app", ['uiGmapgoogle-maps', 'AdalAngular']).config(
+            ['uiGmapGoogleMapApiProvider', function(GoogleMapApiProviders) {
+                GoogleMapApiProviders.configure({
+                    china: true
+                });
+            }])
+            .config(["$httpProvider", ($httpProvider) => {
+                $httpProvider.defaults.headers.common["Accept"] = "application/json; odata=verbose";
+            }])
+            .controller("operationSiteController", Controllers.OperationSiteController)
+        angular.bootstrap(document.getElementById('homeApp'), ['app']);
+    });
 });
