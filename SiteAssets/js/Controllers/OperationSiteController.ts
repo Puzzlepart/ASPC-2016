42481287e9a111dd469d508c96fceabc63fec9fd
olemp <olemp@puzzlepart.com>
2016-02-26 18:18:36 +0100

    Centering map on loc in operation room

--- SiteAssets/js/Controllers/OperationSiteController.ts
+++ SiteAssets/js/Controllers/OperationSiteController.ts
@@ -4,17 +4,24 @@ module Controllers {
     export class OperationSiteController {
         $scope: any;
 
-        constructor($scope, $graphService : Services.GraphService, adalAuthenticationService) {
+        constructor($scope, $graphService : Services.GraphService, $siteService: Services.SiteService) {
             this.$scope = $scope;
             this.$scope.map = {
                 markers: [],
-                selectedMarker: null,
                 center: {
                     latitude: 59.9756579,
                     longitude: 10.6593764
                 },
-                zoom: 3
+                zoom: 8
             };
+            $siteService.getSiteProperties(_spPageContextInfo.webAbsoluteUrl).then((properties:any) => {
+               var loc = properties.PzlLocation ? angular.fromJson(properties.PzlLocation) : null;
+               this.$scope.map.center = loc.coords;
+               this.$scope.map.markers.push({
+                   id: Math.floor(Math.random()*1000),
+                   coords: loc
+               })
+            });
         }
     
     }
