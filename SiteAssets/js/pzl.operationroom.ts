3187ccf60f166b6fdced3f40204b70283e131d62
olemp <olemp@puzzlepart.com>
2016-02-26 17:49:12 +0100

    Adal stuff

--- SiteAssets/js/pzl.operationroom.ts
+++ SiteAssets/js/pzl.operationroom.ts
@@ -2,7 +2,6 @@
 /// <reference path="Controllers\controllers.d.ts" />
 /// <reference path="Services\services.d.ts" />
 
-
 EnsureScriptFunc('adal-angular.min.js', null, function() {
     EnsureScriptFunc('angular-google-maps.min.js', null, function() {
         var app = angular.module("app", ['uiGmapgoogle-maps', 'AdalAngular']).config(
@@ -14,6 +13,18 @@ EnsureScriptFunc('adal-angular.min.js', null, function() {
             .config(["$httpProvider", ($httpProvider) => {
                 $httpProvider.defaults.headers.common["Accept"] = "application/json; odata=verbose";
             }])
+            .config(($httpProvider, adalAuthenticationServiceProvider) => {
+                adalAuthenticationServiceProvider.init(
+                    {
+                        clientId: 'f5b7a175-7677-4496-9a78-282755efba3f',
+                        endpoints: {
+                            'https://graph.microsoft.com': 'https://graph.microsoft.com'
+                        }
+                    },
+                    $httpProvider
+                );
+            })
+            .service("$graphService", Services.GraphService)
             .controller("operationSiteController", Controllers.OperationSiteController)
         angular.bootstrap(document.getElementById('homeApp'), ['app']);
     });
